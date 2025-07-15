1c9cd1e942d4af6874161fd7b2f2ece37e5bee50
Utkutugsal <tugsalutku@hotmail.com.tr>
2025-07-15 16:40:52 +0300

    Fix authenticateToken import destructuring

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { Database } = require('../config/database');
-const authenticateToken = require('../middleware/auth');
+const { authenticateToken } = require('../middleware/auth');
 
 const db = new Database();
 
